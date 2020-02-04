c97d61eab9c7eef15cf458b8d2feccfb6b91de34
Evgeny <Kyberbote@gmail.com>
2020-02-04 14:07:35 +0300

    fixing tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,7 +2,7 @@
 #include "Automata.h"
 
 TEST(lab4, balance_test1) 
-{Am I even doing it right
+{
     Automata hotdrinks;
 	
 	hotdrinks.on();
@@ -34,39 +34,5 @@ TEST(lab4, state_test1)
 	EXPECT_EQ(expected, current_state);	
 }
 
-TEST(lab4, state_test2) { 
-	Automata hotdrinks;	
-	hotdrinks.on();
-	hotdrinks.coin(15);
-	std::string current_state = hotdrinks.getState();
-	sdt::string expected = "ACCEPT";
-	EXPECT_EQ(expected, current_state);
-	
-}
-
-TEST(lab4, state_test3){ 
-	Automata hotdrinks;	
-	hotdrinks.on();
-	hotdrinks.coin(15);
-	hotdrinks.cancel();
-	std::string current_state = hotdrinks.getState();
-	sdt::string expected = "WAIT";
-	EXPECT_EQ(expected, current_state);
-	
-}
-
-
-
-TEST(lab4, state_test4){ 
-	Automata hotdrinks;	
-	hotdrinks.on();
-	hotdrinks.printMenu();
-	hotdrinks.coin(15);
-	hotdrinks.coin(10);
-	hotdrinks.choice(7);
-	std::string current_state = hotdrinks.getState();
-	sdt::string expected = "WAIT";
-	EXPECT_EQ(expected, current_state);
-}
 
 
